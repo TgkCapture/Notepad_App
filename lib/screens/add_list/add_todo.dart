e5c9867145a11bcba5458ab5288fc8ee939710cf
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-05-21 12:42:25 +0200

    to_do list file added

---
+++ lib/screens/add_list/add_todo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Addtodo extends StatefulWidget {
+  const Addtodo({ Key? key }) : super(key: key);
+
+  @override
+  _AddtodoState createState() => _AddtodoState();
+}
+
+class _AddtodoState extends State<Addtodo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
