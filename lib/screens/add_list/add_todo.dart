276df3a12f4cda3d55ba55ac888761f554311792
tgkcapture <71700867+TgkCapture@users.noreply.github.com>
2022-05-21 13:09:21 +0200

    appbar for to-do page

--- lib/screens/add_list/add_todo.dart
+++ lib/screens/add_list/add_todo.dart
@@ -12,6 +12,37 @@ class _AddtodoState extends State<Addtodo> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        iconTheme: const IconThemeData(color: Colors.black),
+        actionsIconTheme: const IconThemeData(color: Colors.black),
+        actions: [
+          IconButton(
+          onPressed: () {}, 
+          icon: const Icon(
+            Icons.push_pin_outlined,
+          ),
+          ),
+          IconButton(
+          onPressed: () {}, 
+          icon: const Icon(
+            Icons.notifications_outlined,
+          ),
+          ),
+          IconButton(
+          onPressed: () {}, 
+          icon: const Icon(
+            Icons.dashboard_outlined,
+          ),
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
     );
   }
 }
